bd69d0d8cb2dc4afccb78f79b03fec5e7ee0c88f
DvdQzd <david.quezada.m@gmail.com>
2022-10-25 21:05:48 -0300

    clase 28

---
+++ semana-14-entorno-y-procesos/ejemplos/4-process/main.js
@@ -0,0 +1,39 @@
+/* ------------------------------------------------------- */
+/*   GLOBAL PROCESS: process ( Objeto global de Node.js)   */
+/* ------------------------------------------------------- */
+
+// 1) Eventos
+// 2) Propiedades
+// 3) Métodos
+
+// 1)
+console.log('--------------- Process Events -------------------')
+process.on('exit', code => {
+    setTimeout(() => {
+        console.log('Esto no corre')
+    }, 0)
+    console.log('Salida con código: ' + code)
+})
+
+// 2)
+console.log('--------------- Process Properties -------------------')
+process.stdout.write('Hola\n')
+process.stdout.write('Como\n')
+process.stdout.write('Están!\n')
+
+process.argv.forEach((arg, index) => {
+    console.log(index + ' -> ' + arg)
+});
+console.log('execPath', process.execPath)
+console.log('platform',process.platform)
+console.log('pid',process.pid)
+console.log('Versión actual', process.version)
+
+
+// 3)
+console.log('--------------- Process Methods -------------------')
+console.log('Carpeta corriente:', process.cwd())
+console.log('Uso de memoria', process.memoryUsage())
+process.exit(9)
+
+console.log('Programa terminado')
--- semana-14-entorno-y-procesos/ejemplos/ejemploClase.js
+++ semana-14-entorno-y-procesos/ejemplos/ejemploClase.js
@@ -6,8 +6,18 @@
 
 // console.log(process.argv.slice(2))
 
-console.log('Variables de entorno: ', process.env)
+// console.log('Variables de entorno: ', process.env)
 
-// hola mundo
+// // hola mundo
 
-console.log(`Hola ${process.env.HOLA}`)
+// console.log(`Hola ${process.env.HOLA}`)
+
+const { exec } = require('child_process');
+
+exec('ls -lh', (err, stdout, stderr) => {
+    if (err) {
+        console.error('hay un error:', err);
+        return;
+    }
+    console.log(stdout);
+});
