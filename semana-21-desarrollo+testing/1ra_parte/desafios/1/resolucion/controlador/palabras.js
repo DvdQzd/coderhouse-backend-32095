9b09be1298dea259d0b775d1e5286f835a453dbb
David Quezada <david.quezada.m@gmail.com>
2022-12-22 21:14:53 -0300

    clase 41

---
+++ semana-21-desarrollo+testing/1ra_parte/desafios/1/resolucion/controlador/palabras.js
@@ -0,0 +1,36 @@
+import ApiPalabras from '../api/palabras.js'
+
+class ControladorPalabras {
+
+    constructor() {
+        this.apiPalabras = new ApiPalabras()
+    }
+
+    obtenerPalabras = async (req,res) => {
+        try {
+            let id = req.params.id
+            //console.log(id)
+            let palabras = await this.apiPalabras.obtenerPalabras(id)
+
+            res.send(palabras)
+        }
+        catch(error) {
+            console.log('error obtenerPalabras', error)
+        }
+    }
+
+    guardarPalabra = async (req,res) => {
+        try {
+            let palabra = req.body
+            //console.log(Palabra)
+            let palabraGuardada = await this.apiPalabras.guardarPalabra(palabra)
+
+            res.json(palabraGuardada)
+        }
+        catch(error) {
+            console.log('error obtenerPalabras', error)
+        }
+    }
+}
+
+export default ControladorPalabras
